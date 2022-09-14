db8852b18065e1b03828d16636ce84efdd8e3854
caleb-iliff <112203387+caleb-iliff@users.noreply.github.com>
2022-09-14 14:40:36 -0500

    Saving... Done?

--- logs.py
+++ logs.py
@@ -5,6 +5,10 @@
 import math
 
 def g(x):
+    if x < -10 or x > 7 :
+        print("Not within Range.")
+        exit()
+
     return math.log(100 - x**2) + math.sqrt(84 - 5*x - x**2)
 
 x = input("Input the value for x: ")
